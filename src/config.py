4fd0f9fdf5b5d8a2f3b27739e7e42b0e743ff217
m1htan <ngminhtan0410@gmail.com>
2025-11-09 17:44:31 +0700

    Add ACL conference papers (raw PDFs)
    Added a large collection of raw PDF files for ACL conference papers to the data/papers_raw directory. This expands the dataset for further processing and analysis of computational linguistics literature.

--- src/config.py
+++ src/config.py
@@ -2,7 +2,11 @@ import os
 from dotenv import load_dotenv
 
 # Tải biến môi trường
+<<<<<<< Updated upstream
 load_dotenv(dotenv_path='D:\Github\Final_DL_Codex\config\.env')
+=======
+load_dotenv(dotenv_path='D:\Github\Final_DL\config\.env')
+>>>>>>> Stashed changes
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GEMINI_MODEL = os.getenv("GEMINI_MODEL")
